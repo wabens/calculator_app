1b4eba694a5b05600f2b1073ad115f24a9761340
wabens <waltkbenson@gmail.com>
2019-06-26 13:29:23 -0500

    POST and GET verified with Postman

--- server/router/routes.js
+++ server/router/routes.js
@@ -3,16 +3,36 @@ const pool = require('../modules/pool');
 
 
 // Route to GET calculator history
+// http://localhost:5000/calculator
 router.get( '/', ( req, res ) => {
-  pool.query(`SELECT * FROM "history"`)
+  pool.query(`SELECT * FROM "history" ORDER BY "time" ASC LIMIT 10;`)
     .then((result) => {
       res.send(result.rows);
     })
     .catch((error) => {
       console.log(`Error getting calculator expressions`, error);
-      alert(`Sorry, there was an error getting the calculator history. Please try again later.`)
       res.sendStatus(500);
     })
-})
+});
+
+// Route to POST expression
+router.post( '/', (req, res) => {
+  const expression = req.body;
+  const values = [
+    expression.operand1, 
+    expression.operand2,
+    expression.operator,
+    expression.solution,
+  ]
+  
+  pool.query(`INSERT INTO "history" ("operand1", "operand2", "operator", "solution") VALUES ($1, $2, $3, $4);`, values )
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error adding calculator expression to history`, error);
+      res.sendStatus(500);
+    })
+});
 
 module.exports = router;
