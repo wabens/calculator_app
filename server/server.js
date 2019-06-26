39cd19c80bf729c8e6d54af0ec85f5df01ecd307
wabens <waltkbenson@gmail.com>
2019-06-26 12:28:52 -0500

    server up and client are running

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 5000;
+
+const calculator = require('./router/routes');
+
+app.use(express.static('./server/public'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use('/calculator', calculator);
+
+app.listen(PORT, () => {
+  console.log('Now listening on port: ', PORT);
+});
