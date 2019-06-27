26a8ca89d922dcf4510ed791f0fc1178bcbfbac9
wabens <waltkbenson@gmail.com>
2019-06-26 23:41:50 -0500

    more porcess.env stuff

--- server/server.js
+++ server/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 const calculator = require('./router/routes');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 app.use(express.static('./server/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
